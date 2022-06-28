99e71cabe79cdeee99e831140614f45dbeb9f352
unknown <bpragathi24@gmail.com>
2022-06-28 12:09:46 +0530

    adding create offer

---
+++ src/main/java/com/qa/hap/pages/EmployeeMaster.java
@@ -0,0 +1,11 @@
+package com.qa.hap.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.qa.hap.utils.ElementUtil;
+
+public class EmployeeMaster {
+	private WebDriver driver;
+	private ElementUtil eleutil;
+
+}
